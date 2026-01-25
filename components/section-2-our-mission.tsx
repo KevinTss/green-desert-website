60c8dce93141bcdd079db732177317a4021b0c1f
KevinTss <kevin.tassi@gmail.com>
2026-01-25 13:01:56 +0100

    Hide logo caroussel

--- components/section-2-our-mission.tsx
+++ components/section-2-our-mission.tsx
@@ -1,19 +1,23 @@
-import { useContent } from "@/components/language-provider"
-import { PartnersCarousel } from "@/components/partners-carousel"
-import { ProcessExplainer } from "@/components/process-explainer"
-import { Badge, Text as TypographyText } from "@/components/typography"
-import { Section } from "@/components/section"
+import { useContent } from "@/components/language-provider";
+// import { PartnersCarousel } from "@/components/partners-carousel"
+import { ProcessExplainer } from "@/components/process-explainer";
+import { Badge, Text as TypographyText } from "@/components/typography";
+import { Section } from "@/components/section";
 
 export const Section2OurMission = () => {
-  const { home } = useContent()
-  const story = home?.story
+  const { home } = useContent();
+  const story = home?.story;
 
   return (
-    <Section id="story" className="relative overflow-hidden flex items-center min-h-[60vh]">
+    <Section
+      id="story"
+      className="relative overflow-hidden flex items-center min-h-[60vh]"
+    >
       <div className="container mx-auto px-4">
-        <PartnersCarousel partners={story?.partners} />
+        {/* <PartnersCarousel partners={story?.partners} /> */}
         {story && (
-          <div className="max-w-3xl mt-32">
+          // <div className="max-w-3xl mt-32">
+          <div className="max-w-3xl">
             {!!story.badge && <Badge>{story.badge}</Badge>}
             {!!story.body && (
               <TypographyText size="lg" className="mt-3">
@@ -25,5 +29,5 @@ export const Section2OurMission = () => {
         {!!story.process && <ProcessExplainer process={story.process} />}
       </div>
     </Section>
-  )
-}
+  );
+};
