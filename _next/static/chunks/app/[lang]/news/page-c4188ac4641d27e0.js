8dedf68a92d6faf76609dc50200bc8b20060338f
KevinTss <KevinTss@users.noreply.github.com>
2026-02-06 20:28:07 +0000

    deploy: 02c031ddc5bee53c1a39c836d4bc3bb9d7a9bc37

---
+++ _next/static/chunks/app/[lang]/news/page-c4188ac4641d27e0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[267],{9650:(e,s,n)=>{Promise.resolve().then(n.bind(n,2830)),Promise.resolve().then(n.bind(n,2982)),Promise.resolve().then(n.t.bind(n,2317,23)),Promise.resolve().then(n.t.bind(n,6434,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[664,626,133,22,575,358],()=>s(9650)),_N_E=e.O()}]);
