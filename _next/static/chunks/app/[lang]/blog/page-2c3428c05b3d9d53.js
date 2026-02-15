7bccaf12321b664579ee05c8517473a4f993979a
KevinTss <KevinTss@users.noreply.github.com>
2026-02-15 10:14:49 +0000

    deploy: 474fdf7b542a53a9f9707e08614a3044a26f7a0a

--- _next/static/chunks/app/[lang]/blog/page-2c3428c05b3d9d53.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[760],{51:(e,s,n)=>{Promise.resolve().then(n.bind(n,2830)),Promise.resolve().then(n.bind(n,2982)),Promise.resolve().then(n.t.bind(n,2317,23)),Promise.resolve().then(n.t.bind(n,6434,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[664,626,133,22,575,358],()=>s(51)),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/[lang]/research/page-56a91ed468055391.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[589,686],{7294:(e,s,n)=>{Promise.resolve().then(n.bind(n,2830)),Promise.resolve().then(n.bind(n,2982))}},e=>{var s=s=>e(e.s=s);e.O(0,[933,913,626,133,22,575,358],()=>s(7294)),_N_E=e.O()}]);
